d457c4c69e65d1feb12d5c12354392129a1676d9
Endryus-Montezano <endryusmontezano@live.com>
2025-07-23 19:52:50 -0300

    Trabalhando com estruturas condicionais (if, else if, else)

---
+++ Trabalhando Com Estruturas Condicionais (if, else if, else)/Else if/script.js
@@ -0,0 +1,9 @@
+let nivelDeFome = 2
+
+if(nivelDeFome === 1) {
+    console.log("pouca fome")
+} else if(nivelDeFome === 2) {
+    console.log("muita fome")
+}else {
+    console.log("morto de fome")
+}
---
+++ Trabalhando Com Estruturas Condicionais (if, else if, else)/Else/script.js
@@ -0,0 +1,12 @@
+let possuiOvos = false
+
+let itensComprados = ""
+
+if(possuiOvos) { //if(se)
+    itensComprados = "Leite"
+} else { //else(se não)
+    console.log("Passou na sessão de congelados")
+    itensComprados = "Lasanha congelados"
+}
+
+console.log(`item comprado: ${itensComprados}`)
