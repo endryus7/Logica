baba1bd78eab1b06b546b88741ebfb93575be109
Endryus-Montezano <endryusmontezano@live.com>
2025-09-10 16:46:30 -0300

    Revisando operadores aritméticos

---
+++ 3-Operadores/Operadores aritméticos/script.js
@@ -0,0 +1,58 @@
+let idade = 30 
+console.log(idade)
+
+idade = 30 + 6
+console.log(idade)
+
+idade = 30 - 6
+console.log(idade)
+
+let primeiroNumero = 1023
+let segundoNumero = 23
+console.log(primeiroNumero - segundoNumero)
+
+// Multiplicação
+let precoProduto = 100.99
+let valorComTxa = precoProduto * 2
+console.log('Multiplicação: ' + valorComTxa)
+
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador * multiplicando
+console.log('Multiplicação: ' + produto)
+
+
+// Divisão
+let notaDoMercado = 50
+let pessoasParaDividir = 2
+console.log('Divisão: ' + notaDoMercado / pessoasParaDividir)
+
+// Módulo (resto)
+let calculo = 10 % 3
+console.log(`Módulo: ${calculo}`)
+
+
+let contador1 = 1
+// Incremento '++'
+contador1++
+contador1++
+contador1 = contador1 + 1 // é a mesma coisa
+console.log(`Incremento: ${contador1}`)
+
+
+let contador2 = 4
+// Decremento '--'
+contador2--
+contador2--
+console.log(`Decremento: ${contador2}`)
+
+
+// Calcular e atribuir (adianta o sinal antes do '=')
+let preco = 10
+preco += 5 // preco = preco + 5
+preco -= 5 // preco = preco - 5
+console.log(preco)
+
+// Definindo Escopos
+let resultado = 2 * ((5 + 5) - 10)
+console.log(resultado)
